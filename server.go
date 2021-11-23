f43c3ac7edf00230ed63e7129caa14ccaa5b60e9
Meg <ai285063@gmail.com>
2021-11-23 18:04:21 +0800

    add getusers

--- server.go
+++ server.go
@@ -5,21 +5,37 @@ import (
 	"log"
 	"net"
 
-	pb "github.com/ai285063/member_app_gRPC/proto/memberApp"
+	memberApp "github.com/ai285063/member_app_gRPC/proto/memberApp"
 	grpc "google.golang.org/grpc"
 )
 
 const port = ":8080"
 
 type server struct {
-	pb.UnimplementedCRUDServer
+	memberApp.UnimplementedCRUDServer
 }
 
-func (s *server) GetUsers(ctx context.Context, in *pb.GetUsersRequest) (*pb.GetUsersResponse, error) {
-
-	res := &pb.GetUsersResponse{
-		// Users: users,
+func (s *server) GetUsers(ctx context.Context, in *memberApp.GetUsersRequest) (*memberApp.GetUsersResponse, error) {
+	var users []*memberApp.User
+	if err := MysqlDB.Table("users").Find(&users).Error; err != nil {
+		panic(err)
 	}
+
+	// manually cast *gorm to *memberApp.User
+	usersArr := make([]*memberApp.User, 0)
+
+	for _, u := range users {
+		usersArr = append(usersArr, &memberApp.User{
+			Id:       u.Id,
+			Account:  u.Account,
+			Email:    u.Email,
+			Password: u.Password,
+		})
+	}
+	res := &memberApp.GetUsersResponse{
+		Users: usersArr,
+	}
+
 	return res, nil
 }
 
@@ -32,7 +48,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterCRUDServer(s, &server{})
+	memberApp.RegisterCRUDServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
